a158c32a58bbac103b9cee98e1f664858244e1bc
xyytgae <xyytgae@yahoo.co.jp>
2023-01-18 14:28:34 +0900

    feat: #1 タスク作成router実装

--- api/routers/task_router.py
+++ api/routers/task_router.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 
 from api.db.database import get_db
 from api.db.task_datasource import get_task
+from api.crud.task import create_task
+from api import schemas
 
 router = APIRouter()
 
@@ -13,3 +15,8 @@ router = APIRouter()
 def get_contexts(id: int, db: Session = Depends(get_db),):
     """Get contexts."""
     return get_task(db=db, id=id)
+
+@router.post('/tasks', response_model=schemas.TaskCreateResponse)
+async def create_contexts(task_body: schemas.TaskCreate, db: Session = Depends(get_db),):
+    """Create contexts."""
+    return await create_task(db, task_body)
