6ebf2affe975716855e06f924415891a55934390
xyytgae <xyytgae@yahoo.co.jp>
2023-01-19 16:47:49 +0900

    feat: #4 タスク更新router実装

--- api/routers/task_router.py
+++ api/routers/task_router.py
@@ -6,7 +6,7 @@ from typing import List, Union
 
 from api.db.database import get_db
 from api.db.task_datasource import get_task
-from api.crud.task import create_task, get_tasks
+from api.crud.task import create_task, get_tasks, update_task
 from api import schemas
 from api.models.status import Status
 
@@ -27,3 +27,8 @@ def create_contexts(task_body: schemas.TaskCreate, db: Session = Depends(get_db)
 def get_contexts(due_date: Union[str, None] = None, status: Union[Status, None] = None, db: Session = Depends(get_db),):
     """Get contexts."""
     return get_tasks(db, due_date, status)
+
+@router.put('/tasks/{id}', response_model=schemas.TaskUpdateResponse)
+def update_contexts(id: int, task_body: schemas.TaskUpdate, db: Session = Depends(get_db)):
+    """Update contexts."""
+    return update_task(db, id, task_body)
